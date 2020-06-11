efb4d9778d6d54b5db56894874a45881fc8368f9
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2020-06-11 12:15:52 -0300

    [ADDED] toInt, toChar, toFloat, toDouble, toString, operator++ (assuming is a int), toLong, toLongLong. [ADDED] iterable data, and associative data. [ADDED] size() method to get the overall size, and length() method for the iterable data. [ADDED] std::to_string(Var v). [ADDED] many code samples for this little tests.

--- Var.cpp
+++ Var.cpp
@@ -10,10 +10,12 @@ class VarData {
 	VarData() {
 		data = NULL;
 	}
+
 	template<typename DataType>	
 	VarData(DataType v) {
 		data = (int*)new DataType(v);
 	}
+
 	~VarData() {
 		if(data) delete data;
 		data = NULL;
@@ -33,8 +35,9 @@ class VarData {
 };
 
 class Var {
-	map<uint, Var> b;
-	VarData data;
+	VarData data; // data from construction or assignment
+	map<string, Var> adata; // associative data
+	map<uint, Var> idata; // iterable data
 	
 	public:
 	Var(){};
@@ -43,8 +46,20 @@ class Var {
 	Var(DataType v) {
 		data = (DataType) v;
 	}
+
 	Var &operator[](const uint el) {
-		return b[el];
+		return idata[el];
+	}
+	Var &operator[](const int el) {
+		return idata[el];
+	}
+
+	Var &operator[](const string el) {
+		return adata[el];
+	}
+
+	Var &operator[](const char* el) {
+		return adata[el];
 	}
 	
 	template<typename DataType>	
@@ -57,8 +72,57 @@ class Var {
 	operator DataType&() {
 		return (DataType&)(data);
 	}
+
+	int &toInt() {
+		return static_cast<int&>(data);
+	}
+
+	float &toFloat() {
+		return static_cast<float&>(data);
+	}
+
+	double &toDouble() {
+		return static_cast<double&>(data);
+	}
+
+	char &toChar() {
+		return static_cast<char&>(data);
+	}
+
+	long &toLong() {
+		return static_cast<long&>(data);
+	}
+
+	long long  &toLongLong() {
+		return static_cast<long long &>(data);
+	}
+
+	string  &toString() {
+		return static_cast<string &>(data);
+	}
 	
+	int& operator++(int) {
+		toInt()++;
+		return static_cast<int&>(data);
+	}
+
+	size_t size() {
+		size_t total_size = idata.size()
+						  + adata.size();
+		return total_size;
+	}
+
+	size_t length() {
+		return idata.size();
+	}
 };
+
+namespace std {
+	string to_string(Var v) {
+		return static_cast<string&>(v);
+	}
+}
+typedef Var var;
 
 int main () {
 	Var s;
@@ -67,12 +131,39 @@ int main () {
 	s[0][1][2]= false;
 	s[2][3][3][3][4][5][6][10] = 5656;
 	s[3][1]= (char) 'k';
-	s[3][123] = (string)"draweawe";
+	s[3][123] = (string) "draweawe";
 	char as = s[3][1];
 	int af = s[1];
 	string str = s[3][123];
 	Var b = 42;
 	cout << "b : " << (int) b << endl;
+	var hello = string("Hello World");
+	var i = 0;
+	var limit = 3;
+
+
+	cout << hello.toString() 		<< endl;
+	cout << std::to_string(hello) 	<< endl;
+
+	while (i.toInt() < limit.toInt()) {
+		cout << i.toInt() << endl;
+		i++;
+	}
+	
+	var points;
+	for (var k = 0; k.toInt() < 3; k++) {
+		points[k.toInt()]["x"] = k.toInt()*1;
+		points[k.toInt()]["y"] = k.toInt()*1;
+		points[k.toInt()]["z"] = k.toInt()*1;
+	}
+
+	for (var l = 0; l.toInt() < points.length(); l++) {
+		cout 
+		<< "(" << points[l.toInt()]["x"].toInt()
+		<< " " << points[l.toInt()]["y"].toInt()
+		<< " " << points[l.toInt()]["z"].toInt()
+		<< ")" << endl;
+	}
 	
 	cout << "s[1]: " << af << endl;
 	cout << "s[2][2]: " << (float)s[2][2]<< endl;
